86809727e8963de4bd0eb9512bb284981bcab742
carlogambi <carlogambi@hotmail.it>
2021-04-06 20:09:58 +0200

    removed apex from pin descriptor

--- src/components/PositionPin.js
+++ src/components/PositionPin.js
@@ -1,33 +1,33 @@
-import React from 'react'
-import { Marker, Popup } from 'react-leaflet'
-import changeLocationEvent from '../custom-events/changeLocation'
-import customIcons from './customIcons'
+import React from 'react';
+import { Marker, Popup } from 'react-leaflet';
+import changeLocationEvent from '../custom-events/changeLocation';
+import customIcons from './customIcons';
 
-const PositionPin = ({position, info, onClick}) => {
-    // const [isOver, setIsOver] = useState(false)
+const PositionPin = ({ position, info, onClick }) => {
+  // const [isOver, setIsOver] = useState(false)
 
-    return (
-        <Marker 
-            position={position} 
-            icon={customIcons.defaultIcon}
-            eventHandlers={{
-                click: (e) => {
-                    changeLocationEvent.trigger({
-                        payload: {position, info}
-                    })
-                    onClick()
-                },
-                // mouseover: () => setIsOver(true),
-                // mouseout: () => setIsOver(false),
-            }}
-        >
-        <Popup>
-          <div>
-            <h3 style={{width: '100px', }}>{info.title}</h3>
-          </div>
-        </Popup>
-      </Marker>
-    )
-}
+  return (
+    <Marker
+      position={position}
+      icon={customIcons.defaultIcon}
+      eventHandlers={{
+        click: (e) => {
+          changeLocationEvent.trigger({
+            payload: { position, info },
+          });
+          onClick();
+        },
+        // mouseover: () => setIsOver(true),
+        // mouseout: () => setIsOver(false),
+      }}
+    >
+      <Popup>
+        <div>
+          <h3 style={{ width: '100px' }}>{info.title.replace(/"/gm, '')}</h3>
+        </div>
+      </Popup>
+    </Marker>
+  );
+};
 
-export default PositionPin
+export default PositionPin;

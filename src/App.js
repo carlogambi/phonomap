0e6500403e5608d6974175609fdc5f111108f192
carlogambi <carlogambi@hotmail.it>
2021-04-06 19:48:36 +0200

    added first real data

--- src/App.js
+++ src/App.js
@@ -1,29 +1,30 @@
-import React, {useEffect, useState} from 'react'
-import BGmap from './components/BGmap'
-import LogoPhonomap from './components/LogoPhonomap'
-import PhonoContainer from './components/PhonoContainer'
-import initAnimationManager from  './custom-events/initAnimationStep'
-import positionQuery from './custom-events/positionsQuery'
-import exampleDataPositions from './utils/example-data.json'
+import React, { useEffect, useState } from 'react';
+import BGmap from './components/BGmap';
+import LogoPhonomap from './components/LogoPhonomap';
+import PhonoContainer from './components/PhonoContainer';
+import initAnimationManager from './custom-events/initAnimationStep';
+import positionQuery from './custom-events/positionsQuery';
+import exampleDataPositions from './utils/example-data.json';
 
 //animazione logo
-const transitionTime = 0.7
-const timingFunction = 'cubic-bezier(1, 0.01, 0.22, 0.95)'
+const transitionTime = 0.7;
+const timingFunction = 'cubic-bezier(1, 0.01, 0.22, 0.95)';
 const logoStyle = {
   width: '150px',
   position: 'absolute',
   left: '50%',
   top: '88%',
-  transition: `left ${transitionTime}s ${timingFunction}, top ${transitionTime}s ${timingFunction}, width ${transitionTime}s ${timingFunction}`
-}
+  transition: `left ${transitionTime}s ${timingFunction}, top ${transitionTime}s ${timingFunction}, width ${transitionTime}s ${timingFunction}`,
+};
 
 const initLogoStyle = {
   width: '400px',
   position: 'absolute',
   left: '50%',
   top: '50%',
-}
-true && alert(`
+};
+false &&
+  alert(`
 !*!-SITO ANCORA IN FASE DI SVILUPPO-!*!
     Per una corretta visualizzazione 
     aprire questa pagina
@@ -31,45 +32,65 @@ true && alert(`
     working on:
      - animazioni onload
      - ottimizzazione svg/immagini
-`)
+`);
 const App = () => {
-  const [animationSteps, setAnimationSteps] = useState({})
-  const [positions, setPositions] = useState(exampleDataPositions)
-  const authorList = [...new Set(exampleDataPositions.map(p => p.author))]
+  const [animationSteps, setAnimationSteps] = useState({});
+  const [positions, setPositions] = useState(exampleDataPositions);
+  const authorList = [...new Set(positions.map((p) => p.author))];
   useEffect(() => {
-    initAnimationManager.interceptInitAnimation((e) => e !== animationSteps && setAnimationSteps(e))
+    initAnimationManager.interceptInitAnimation(
+      (e) => e !== animationSteps && setAnimationSteps(e)
+    );
     positionQuery.intercept((e) => {
       switch (e.detail.field) {
         case 'author':
-            if(e.detail.query === 'ALL AUTHORS'){
-              setPositions(exampleDataPositions)
-            }else{
-            const filtered = exampleDataPositions.filter(p => p.author === e.detail.query)
-            setPositions(filtered)
-            }
-          break
+          if (e.detail.query === 'ALL AUTHORS') {
+            setPositions(exampleDataPositions);
+          } else {
+            const filtered = exampleDataPositions.filter(
+              (p) => p.author === e.detail.query
+            );
+            setPositions(filtered);
+          }
+          break;
         default:
-          break
+          break;
       }
-    })
-  }, [animationSteps, positions])
-  return (<>
-      {true && animationSteps.currentStep !== 6 && <div style={{borderRadius: '10px', position:'absolute',zIndex:100,top:'15px',left:'70px', backgroundColor:'yellow', padding:'10px',fontFamily:'sans-serif'}}>
-        <h5>
-        {animationSteps.currentStep} / {animationSteps.stepsNumber}
-        </h5>
-      </div>}
-      {animationSteps.currentStep >= 0 && 
-        <LogoPhonomap 
-            style={(animationSteps.currentStep >= 2 )?logoStyle:initLogoStyle} 
-            showInfo={false} 
-            setDeviation={(prev) => prev}
+    });
+  }, [animationSteps, positions]);
+  return (
+    <>
+      {true && animationSteps.currentStep !== 6 && (
+        <div
+          style={{
+            borderRadius: '10px',
+            position: 'absolute',
+            zIndex: 100,
+            top: '15px',
+            left: '70px',
+            backgroundColor: 'yellow',
+            padding: '10px',
+            fontFamily: 'sans-serif',
+          }}
+        >
+          <h5>
+            {animationSteps.currentStep} / {animationSteps.stepsNumber}
+          </h5>
+        </div>
+      )}
+      {animationSteps.currentStep >= 0 && (
+        <LogoPhonomap
+          style={animationSteps.currentStep >= 2 ? logoStyle : initLogoStyle}
+          showInfo={false}
+          setDeviation={(prev) => prev}
         />
-        }
-      <BGmap animationSteps={animationSteps} positions={positions}/>
-      {animationSteps.currentStep > 2 && <PhonoContainer authorList={authorList}/>}
-  </>
-  )
-}
+      )}
+      <BGmap animationSteps={animationSteps} positions={positions} />
+      {animationSteps.currentStep > 2 && (
+        <PhonoContainer authorList={authorList} />
+      )}
+    </>
+  );
+};
 
-export default App
+export default App;

5e2e84f30f4331625245c5863e86c5c702a6bed8
carlogambi <carlogambi@hotmail.it>
2021-06-13 13:41:13 +0200

    fixed readme

--- src/components/PopUp.js
+++ src/components/PopUp.js
@@ -13,75 +13,117 @@ const PopUp = ({ visibility }) => {
     <div className={nomeClasse + ' popupContainer'}>
       <div id='popUp'>
         <div id='testoPopUp'>
-          <p>COSA</p>
+          <p>COS'{'è'.toUpperCase()} PHONOMAP?</p>
           <p>
             Phonomap è una mappatura sonora del territorio italiano. Racconta un
             arco temporale esteso attraverso il punto di vista e di ascolto
-            delle persone. Chiunque può contribuire, anche tu. Ogni suono
-            raccolto è a disposizione di chiunque lo voglia ascoltare ed
-            utilizzare. È scaricabile gratuitamente dal profilo Soundcloud di
-            Forevergreen.fm all’apposita{' '}
+            delle persone. Chiunque può contribuire, anche tu.
+            <br />
+            Ogni suono raccolto è a disposizione di chiunque lo voglia ascoltare
+            ed utilizzare. È scaricabile gratuitamente dal profilo Soundcloud di
+            Forevergreen.fm all’apposita playlist .
             <a
               target='blank'
               href='https://soundcloud.com/forevergreenrec/sets/phonomap'
             >
-              {' '}
               playlist Italian Phonomap
             </a>
-            .{' '}
+            .
           </p>
           <br />
+          <p>POSSO FARLO ANCH'IO?</p>
+          <p>
+            Può contribuire chiunque. C’è un suono curioso, particolare, che ha
+            attirato la tua attenzione? Qualcosa che stimola in te un'emozione
+            forte, bella o brutta? Qualcosa a cui ti sei affezionat*? Registralo
+            ed invialo!
+          </p>
           <p>COME PARTECIPARE</p>
           <p>
             Puoi partecipare utilizzando semplicemente il tuo smartphone
             attraverso un’app di registrazione audio. Se sei però in possesso di
+            uno strumento più professionale, sentiti liber* di utilizzarlo!
+          </p>
+          <p>
+            Puoi partecipare utilizzando semplicemente il tuo smartphone
+            attraverso un’app di registrazione audio. Se sei però in possesso di
             uno strumento più professionale, sentiti liber* di utilizzarlo! Ti
-            chiediamo inoltre: <br />
-            - una fotografia del punto in cui effettui la registrazione <br />
+            chiediamo inoltre: - una fotografia del punto in cui effettui la
+            registrazione <br />
             - una descrizione del suono <br />
             - il tuo nome o nickname
             <br />
-            Quì trovi tutte le informazioni necessarie:.
+            Quì trovi tutte le informazioni necessarie:
+            <br />
             <a
               target='blank'
               href='https://docs.google.com/forms/d/e/1FAIpQLSe5oyK8wYeNC5HiKL3HXbsFxopf5UuFi2rP9xd3JM4nRgI75w/viewform'
             >
-              il Form.{' '}
+              Form di partecipazione
             </a>{' '}
           </p>
-          <br />
-          <p>CHI</p>
-          <p>
-            Può contribuire chiunque. C’è un suono curioso, particolare, che ha
-            colto la tua attenzione? Registralo ed invialo!
-          </p>
-          <br />
-          <p>QUANDO</p>
+          <p>C'È UN TERMINE?</p>
           <p>
             La chiamata è sempre aperta! Phonomap è nata il 1 giugno del 2020 e
             speriamo non finisca mai.
           </p>
-          <br />
-          <p>PERCHÈ</p>
+          <p>PERCHÉ ESISTE?</p>
           <p>
-            Tutto il mondo ha dovuto fermarsi nel 2020. Phonomap nasce dall’idea
-            di raccogliere e riordinare, nello spazio della mappa nazionale e
-            dal tempo della pandemia in avanti, qualunque evento sonoro
-            testimone di una inaspettata condizione per cui l'attenzione
-            all'ambiente, come quello (in)sonoro durante il lockdown, si è
-            imposta agli occhi e alle orecchie di tutt*.
+            Tutto il mondo ha, in qualche modo, dovuto fermarsi nel 2020.
+            L'inaspettata condizione creatasi ha stimolato una maggiore
+            attenzione all'ambiente, in particolare quello (in)sonoro del
+            lockdown.
+            <br />
+            Phonomap nasce dall’idea di raccogliere e riordinare nello spazio di
+            una mappa nazionale gli eventi sonori dal tempo della pandemia in
+            avanti.
+            <br />I suoni raccolti possono essere ascoltati, scaricati, remixati
+            e utilizzati da chiunque.
+            <br />
+            Leggi di più sulle licenze{' '}
+            <a
+              target='blank'
+              href='https://creativecommons.org/faq/#what-are-creative-commons-licenses'
+            >
+              Creative Commons
+            </a>{' '}
+            applicate a Phonomap, ovvero “BY” e “NC”.
           </p>
-          <br />
           <hr style={{ borderStyle: 'dashed' }} />
           <br />
           <p>
-            {' '}
-            <a target='blank' href='http://forevergreen.fm/'>
-              Forevergreen.fm
-            </a>{' '}
-            è un’associazione culturale che dal 2012 progetta in campo musicale,
-            teatrale e museale attraverso festival, spettacoli, concerti e
-            workshop.
+            <p>COM'È NATA PHONOMAP?</p>
+            <p>
+              Phonomap è un progetto di{' '}
+              <a target='blank' href='http://forevergreen.fm/'>
+                Forevergreen.fm
+              </a>
+              , un’associazione culturale che dal 2012 progetta in campo
+              musicale, teatrale e museale attraverso festival, spettacoli,
+              concerti e workshop.
+            </p>
+            <p>
+              La scintilla è partita nel 2020 da{' '}
+              <a
+                target='blank'
+                href='http://www.trallaleronline.net/2019/09/25/intervista-a-rinaldo-marti/'
+              >
+                Rinaldo Marti
+              </a>
+              , docente di Musica atipico, dedito all’ascolto meditativo, che ha
+              proposto l’idea di una mappa sonora inclusiva.
+            </p>
+            <p>
+              Lo sviluppo di questa mappa è stato possibile grazie al lavoro
+              dell'artista multiform{' '}
+              <a
+                target='blank'
+                href='http://formeuniche.org/author/carlo-gambirasio/'
+              >
+                Carlo Gambirasio
+              </a>
+              , fondatore di StudioEO a Milano.
+            </p>
           </p>
         </div>
       </div>
